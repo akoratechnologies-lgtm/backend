271721091d27d65f0683df5c0c3d53c3415f6f66
AKORA Technologies <akoratechnologies@gmail.comm>
2026-08-05 16:16:27 +0530

    done

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,17 +1,26 @@
-const admin = require("firebase-admin");
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
 
 let serviceAccount;
 
+// Production (Render)
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  serviceAccount.private_key =
-    serviceAccount.private_key.replace(/\\n/g, "\n");
+
+  // Fix escaped newlines in private key
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 } else {
+  // Local development
   serviceAccount = require("../../firebase-service-account.json");
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const app =
+  getApps().length === 0
+    ? initializeApp({
+        credential: cert(serviceAccount),
+      })
+    : getApps()[0];
 
-module.exports = admin;
+module.exports = {
+  auth: () => getAuth(app),
+};
