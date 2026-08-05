cc0285cc2631beac02289d220417481ac3d445c2
AKORA Technologies <akoratechnologies@gmail.comm>
2026-08-05 16:19:24 +0530

    done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 const userController = require('../controllers/userController');
-const followController = require('../controllers/followController');
+const followController = require('../controllers/Followcontroller');
 
 router.use(protect);
 router.get('/me', userController.getMe);
