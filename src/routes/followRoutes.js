3e7bda87aa50f50546dc89e71a1248b329871780
AKORA Technologies <akoratechnologies@gmail.comm>
2026-08-09 12:36:38 +0530

    fix chat route

--- src/routes/followRoutes.js
+++ src/routes/followRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
-const followController = require("../controllers/followcontroller");
+const followController = require("../controllers/Followcontroller");
 
 router.use(protect);
 
